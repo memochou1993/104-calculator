e87710f744a613879d0b969e23cfcadeecfe3622
William <william@fontrip.com>
2019-05-24 00:08:54 +0800

    新增 Vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
